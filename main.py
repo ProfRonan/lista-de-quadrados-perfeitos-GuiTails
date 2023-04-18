9c9b8cd8e3be506f0788b94b028baf2b560b23aa
Guilherme <128387905+GuiTails@users.noreply.github.com>
2023-04-18 18:15:07 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 print("Digite um número")
 y = int(input(">>> "))
 x = 0
-print(x**2)
-while True:
-    x = x + 1
-    if x < y:
-        print(x**2)
-    if x ==y:
-        print(x**2)
-        break
+if y <=0:
+    print("Número Inválido")
+if y > 0:
+    while True:
+        x = x + 1
+        if x < y:
+            print(x**2)
+        if x ==y:
+            print(x**2)
+            break
